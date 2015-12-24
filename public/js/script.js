eb0da53929b157ac4966b90121c295d17c8088ed
msmichellegar <msmichellegar@gmail.com>
2015-12-24 11:35:00 +1100

    recipe content now displays on click from homepage

--- public/js/script.js
+++ public/js/script.js
@@ -11,9 +11,28 @@ if (window.location.pathname === "/") {
 
 }
 
+// if this is a recipe page
+if (window.location.pathname.split('/')[1] === "recipe") {
+    var recipeId = window.location.pathname.split('/')[2];
+
+    // get recipe data
+    $.ajax('/api/recipe/get/' + recipeId, {
+        success: function(recipeData) {
+            // display recipe
+            displayRecipe(recipeData);
+        }
+    });
+
+}
+
 function displayRecipesGallery (recipes) {
     for (var i=0; i < recipes.length; i++) {
-        var galleryItem = "<div><p>" + recipes[i].title + "</p></div>";
+        var galleryItem = "<a href='/recipe/"+ recipes[i].id + "'><div><p>" + recipes[i].title + "</p></div></a>";
         $("#recipes-gallery").append(galleryItem);
     }
+}
+
+function displayRecipe (recipeData) {
+    var recipeContent = "<div><h1>" + recipeData.title + "</h1><p>" + recipeData.author + "</p><img src='" + recipeData.image + "'<p>" + recipeData.background + "</p><p>" + recipeData.time + "</p><p>" + recipeData.ingredients + "</p><p>" + recipeData.directions + "</p></div>";
+    $("#recipe-content").append(recipeContent);
 }

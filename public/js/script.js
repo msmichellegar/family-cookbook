657f8c1bc94bd9e4fa748478448b0ba9f0fe4b7a
msmichellegar <msmichellegar@gmail.com>
2015-12-29 21:13:40 +1100

    styles recipe page + adds loading gif

--- public/js/script.js
+++ public/js/script.js
@@ -26,23 +26,44 @@ if (window.location.pathname.split('/')[1] === "recipe") {
 }
 
 function displayRecipesGallery (recipes) {
+
+    $("#loading-gif").remove();
+
     for (var i=0; i < recipes.length; i++) {
         var recipeImage;
         var galleryItem;
 
-        if (recipes[i].image) {
+        if (determineIfRecipeHasValidImage(recipes[i]) !== false) {
             recipeImage = recipes[i].image;
         } else {
             recipeImage = "/static/public/images/donuts.png";
         }
 
-        galleryItem = "<a href='/recipe/"+ recipes[i].id + "'><div class='recipe-block'><p>" + recipes[i].title + "</p></div></a>";
+        galleryItem = "<a href='/recipe/"+ recipes[i].id + "'><div class='recipe-block'><img src='"+ recipeImage +"'><p>" + recipes[i].title + "</p><hr></hr><h4>By " + recipes[i].author +"</h4></div></a>";
 
         $("#recipes-gallery").append(galleryItem);
     }
 }
 
+function determineIfRecipeHasValidImage (recipeData) {
+    if (recipeData.image && ((recipeData.image).indexOf("jpg") !== -1 || (recipeData.image).indexOf("png") !== -1)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function displayRecipe (recipeData) {
-    var recipeContent = "<div><h1>" + recipeData.title + "</h1><p>" + recipeData.author + "</p><img src='" + recipeData.image + "'><p>" + recipeData.background + "</p><p>" + recipeData.time + "</p><p>" + recipeData.ingredients + "</p><p>" + recipeData.directions + "</p></div>";
+    var recipeImage;
+    var recipeContent;
+
+    if (determineIfRecipeHasValidImage(recipeData) !== false) {
+        recipeImage = recipeData.image;
+    } else {
+        recipeImage = "/static/public/images/donuts.png";
+    }
+
+    recipeContent = "<div class='recipe'><div class='title'><h2>" + recipeData.title + "</h2><h4>Author: " + recipeData.author + " | Cooking time: " + recipeData.time + "</h4></div><div class='content-row-one'><img src='" + recipeImage + "'><div class='background'><h2>Background</h2><p>" + recipeData.background + "</p></div></div><div class='directions'><h2>Ingredients</h2><p class='ingredients'>" + recipeData.ingredients + "</p><h2>Directions</h2><p>" + recipeData.directions + "</p></div></div>";
+
     $("#recipe-content").append(recipeContent);
 }
